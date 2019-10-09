80c46893baa9243c8926c21317eb389864852ef7
Abdullah Bandan <abdullahbandan@Abdullahs-MacBook-Pro-2.local>
2019-10-09 17:11:03 +0300

    first commit

---
+++ PitchPerfect/ListeningViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListeningViewController.swift
+//  PitchPerfect
+//
+//  Created by Abdullah Bandan on 05/01/1441 AH.
+//  Copyright © 1441 AbdullahBandan. All rights reserved.
+//
+
+import UIKit
+
+class ListeningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PitchPerfect/RecordingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Abdullah Bandan on 05/01/1441 AH.
+//  Copyright © 1441 AbdullahBandan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
